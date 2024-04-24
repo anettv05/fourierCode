7f2a1f8495bacf946b0e9eaa2b074295261d303a
anettv05 <140855219+anettv05@users.noreply.github.com>
2024-04-24 13:59:04 +0530

    Add files via upload

---
+++ audio.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+# Load the audio file
+audio_file = '/Users/anettvarghese/Downloads/Test rec.wav'
+sample_rate, data = wavfile.read(audio_file)
+
+# Convert stereo to mono if the audio is stereo
+if len(data.shape) > 1:
+    data = data.mean(axis=1)
+
+# Compute the Fourier transform
+fourier_transform = np.fft.fft(data)
+
+# Compute the frequencies corresponding to the FFT
+frequencies = np.fft.fftfreq(len(data), 1/sample_rate)
+
+# Plot the Fourier transform magnitude
+plt.figure(figsize=(10, 4))
+plt.title('Fourier Transform of Audio Signal')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Magnitude')
+plt.plot(frequencies[:len(frequencies)//2], np.abs(fourier_transform)[:len(frequencies)//2])
+plt.grid(True)
+plt.show()
